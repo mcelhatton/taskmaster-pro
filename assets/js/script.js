4671d2e806f954908533b8fa2ebe4cb78a662991
mcelhatton <colinm@mobiledealerdata.com>
2022-03-24 09:17:26 -0500

    saving progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,7 @@ var createTask = function(taskText, taskDate, taskList) {
   $("#list-" + taskList).append(taskLi);
 };
 
+
 var loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
 
@@ -45,8 +46,39 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
 
+    var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
 
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function() {
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  tasks[status][index].text = text;
+saveTasks();
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
